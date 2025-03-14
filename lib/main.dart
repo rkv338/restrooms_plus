2004327f795f5b856007e6bf69cd4ae5aa33950e
Rithvik <vikturi99@utexas.edu>
2025-03-14 08:56:40 -0500

    Changes to grab live location from user

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
+import 'package:geolocator/geolocator.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -14,11 +14,71 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   late GoogleMapController mapController;
+  bool _isLoading = true;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  late LatLng _center;
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+  }
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+  getLocation() async {
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+
+    Position position =
+    // await _determinePosition();
+    await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    double lat = position.latitude;
+    double long = position.longitude;
+
+    LatLng location = LatLng(lat, long);
+
+    setState(() {
+      _center = location;
+      _isLoading = false;
+    });
   }
 
   @override
@@ -33,11 +93,12 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Maps Sample App'),
           elevation: 2,
         ),
-        body: GoogleMap(
+        body: _isLoading ?
+        Center(child:CircularProgressIndicator()): GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
-            zoom: 11.0,
+            zoom: 16.0,
           ),
         ),
       ),
